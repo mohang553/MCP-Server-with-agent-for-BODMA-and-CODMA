f37dd01d903edf30d4796985be2a9573e369fe8c
mohang553 <mohanmg553@gmail.com>
2026-02-12 23:21:42 +0530

    Update agent.py

--- agent/agent.py
+++ agent/agent.py
@@ -38,7 +38,8 @@ if not GEMINI_API_KEY:
 
 genai.configure(api_key=GEMINI_API_KEY)
 
-MCP_SERVER_URL = "https://calculator-mcp-74e1.onrender.com/"
+MCP_SERVER_URL = "https://calculator-mcp-74e1.onrender.com/mcp/sse"
+
 
 
 # ============================================================================
